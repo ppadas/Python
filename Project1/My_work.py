2b6259068d60354ad6b15b0d8e27c92fee499991
ppadas <padaso@mail.ru>
2020-05-02 22:47:09 +0300

    Без последнего замечания

--- Project1/My_work.py
+++ Project1/My_work.py
@@ -3,131 +3,156 @@ from tkinter.ttk import Radiobutton
 import datetime
 import time
 from tkinter import messagebox
+from collections import defaultdict
 
 
-def printRools():
+stat_template = '''
+
+{today}
+File: {file}
+Time: {duration} seconds
+Statistics:
+{items}
+'''
+
+stat_item_template = '<{}> - {}\n'
+
+
+def print_rools():
     file = open("rools.txt", "r")
     rools = file.read()
-    messagebox.showinfo('Правила', rools)
+    messagebox.showinfo(titels[1], rools)
 
-def getSymbol(text):
+
+def get_symbol(text):
     for i in text:
         print(i)
         yield i
 
-window = Tk()
-window.geometry('1600x1000')
-window.title("Мой клавиатурный тренажёр")
-filenames = ['text.txt', 'bigText.txt', 'my.txt']
 
-def clicked2():
-    lbl = Label(window, text='New file:', font=("Arial Bold", 10))
-    lbl.grid(column=3, row=5)
-    entry = Entry(window, width=10)
-    entry.grid(column=3, row=6)
-    def clicked3():
-        filenames[selected.get()] = entry.get()
-        lbl.destroy()
-        entry.destroy()
-        button.destroy()
-    button = Button(window, text="Ok", command=clicked3)
-    button.grid(column=3, row=7)
+window = Tk()
+window.geometry('800x500')
+filenames = ['text.txt', 'bigText.txt', 'my.txt']
+titels_file = open('titels.txt', 'r')
+titels = titels_file.readlines()
+titels_file.close()
+for i in range(len(titels)):
+    pair = titels[i].split("\n")
+    titels[i] = pair[0]
+window.title(titels[0])
+
 
 def changeFile():
     rad1 = Radiobutton(window, text='-1', value=0, variable=selected)
     rad2 = Radiobutton(window, text='-2', value=1, variable=selected)
     rad3 = Radiobutton(window, text='-3', value=2, variable=selected)
-    btn = Button(window, text="Выбрать", command=clicked2)
+
+    def clicked2():
+        lbl = Label(window, text=titels[8], font=("Arial Bold", 10))
+        lbl.grid(column=3, row=5)
+        entry = Entry(window, width=10)
+        entry.grid(column=3, row=6)
+
+        def clicked3():
+            file = open(entry.get(), "r")
+            lines = file.readlines()
+            if len(lines) > 1 or len(lines) == 0 or lines[0] == '':
+                lbl_err = Label(window, text=titels[14] + ": " + entry.get(),
+                                font=("Arial Bold", 10))
+                lbl_err.grid(column=3, row=5)
+            else:
+                lbl_done = Label(window, text=titels[15] + ": " + entry.get(),
+                                 font=("Arial Bold", 10))
+                lbl_done.grid(column=3, row=5)
+                filenames[selected.get()] = entry.get()
+            lbl.destroy()
+            entry.destroy()
+            button.destroy()
+
+        button = Button(window, text=titels[3], command=clicked3)
+        button.grid(column=3, row=7)
+
+    btn = Button(window, text=titels[2], command=clicked2)
     rad1.grid(column=3, row=1)
     rad2.grid(column=3, row=2)
     rad3.grid(column=3, row=3)
     btn.grid(column=3, row=4)
 
+
 def clicked():
-    timeB = time.time()
+    time_begin = time.time()
     if selected.get() == 10:
         changeFile()
         return
 
-    fileNow = filenames[selected.get()]
-    with open(fileNow) as f:
+    file_now = filenames[selected.get()]
+    with open(file_now) as f:
         text_ = f.read()
     lbl = Label(window, text=text_, font=("Arial Bold", 12))
     lbl.grid(column=selected.get(), row=2)
-    text = getSymbol(text_)
+    text = get_symbol(text_)
     letter = [next(text)]
 
     def check():
-        textIn = text_
-        textOut = entry.get()
-        print(textIn)
-        print(len(textOut))
-        print(len(textIn))
-        print(textOut)
-        if len(textOut) + 1 == len(textIn):
-            ##messagebox.showinfo('Результат', 'Все верно!\n Статистику и время можно посмотреть в файле ')
-            lbl = Label(window, text='Все верно!\n Статистику и время можно посмотреть в файле ', font=("Arial Bold", 10))
-            timeE = time.time()
-            stat = open('stat.txt', 'a')
-            stat.write("\n\n")
-            stat.write(str(datetime.datetime.today()))
-            stat.write("\n")
-            stat.write("file:")
-            stat.write(fileNow)
-            stat.write("\nTime: ")
-            stat.write(str(timeE - timeB))
-            stat.write("sec")
-            stat.write("\nStatistic:\n")
-            for let, err in statistic.items():
-                stat.write("<")
-                stat.write(let)
-                stat.write(">")
-                stat.write(" - ")
-                stat.write(str(err))
-                stat.write(" ")
-            stat.close()
-            for i in list(statistic):
-                del statistic[i]
+        text_in = text_
+        text_out = entry.get()
+        print(text_in)
+        print(len(text_out))
+        print(len(text_in))
+        print(text_out)
+        if len(text_out) + 1 == len(text_in):
+            if len(statistic) == 0:
+                lbl = Label(window, text=titels[13] + '\n' + titels[11],
+                            font=("Arial Bold", 10))
+            else:
+                lbl = Label(window, text=titels[10] + '\n' + titels[11],
+                            font=("Arial Bold", 10))
+                time_end = time.time()
+                items = ''.join(stat_item_template.format(let, err) for let,
+                                err in statistic.items())
+                stat = stat_template.format(today=datetime.datetime.today(),
+                                            file=file_now,
+                                            duration=str(time_end -
+                                                         time_begin),
+                                            items=items)
+                print(items)
+                with open('stat.txt', 'a') as stat_file:
+                    stat_file.write(stat)
+                for i in list(statistic):
+                    del statistic[i]
         else:
-            ##messagebox.showinfo('Результат', 'Нужно дописать текст')
-            lbl = Label(window, text='Еще немного!', font=("Arial Bold", 10))
+            lbl = Label(window, text=titels[12], font=("Arial Bold", 10))
         lbl.grid(column=selected.get(), row=5)
 
-
-    buttonOk = Button(window, text="Проверка", command=check)
-    buttonOk.grid(column=selected.get(), row=4)
-    entry = Entry(window, validate="key")
+    button_ok = Button(window, text=titels[9], command=check)
+    button_ok.grid(column=selected.get(), row=4)
+    entry = Entry(window, validate="key", width=50)
     entry.grid(column=selected.get(), row=3)
     entry.focus()
 
-    def testVal(inStr):
-        if inStr[len(inStr) - 1] != letter[0]:
-            if letter[0] in statistic:
-                a = statistic .get(letter[0])
-                statistic .update({letter[0]: a+1})
-            else:
-                statistic .update({letter[0]: 1})
+    def test_val(in_str):
+        if in_str[-1] != letter[0]:
+            statistic[letter[0]] += 1
             return False
-        letter.pop()
-        letter.append(next(text))
+        letter[0] = next(text)
         return True
 
-    entry['validatecommand'] = (entry.register(testVal), '%P')
+    entry['validatecommand'] = (entry.register(test_val), '%P')
 
 selected = IntVar()
-rad1 = Radiobutton(window, text='Малая тренировка', value=0, variable=selected)
-rad2 = Radiobutton(window, text='Большой текст', value=1, variable=selected)
-rad3 = Radiobutton(window, text='Мой Файл', value=2, variable=selected)
-rad4 = Radiobutton(window, text='Сменить файл', value=10, variable=selected)
-btn = Button(window, text="Выбрать", command=clicked)
+rad1 = Radiobutton(window, text=titels[4], value=0, variable=selected)
+rad2 = Radiobutton(window, text=titels[5], value=1, variable=selected)
+rad3 = Radiobutton(window, text=titels[6], value=2, variable=selected)
+rad4 = Radiobutton(window, text=titels[7], value=10, variable=selected)
+btn = Button(window, text=titels[2], command=clicked)
 rad1.grid(column=0, row=0)
 rad2.grid(column=1, row=0)
 rad3.grid(column=2, row=0)
 rad4.grid(column=3, row=0)
 btn.grid(column=4, row=0)
-btn2 = Button(window, text="Правила", command=printRools)
+btn2 = Button(window, text=titels[1], command=print_rools)
 btn2.grid(column=10, row=10)
 
-statistic = {}
+statistic = defaultdict(int)
 
 window.mainloop()
