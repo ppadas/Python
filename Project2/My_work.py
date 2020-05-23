c3d2ad992012683ccc1657c577e4158faea8158c
ppadas <padaso@mail.ru>
2020-05-24 02:44:30 +0300

    Исправила + запустила

--- Project2/My_work.py
+++ Project2/My_work.py
@@ -1,53 +1,48 @@
 import telebot
-bot = telebot.TeleBot('1070560085:AAEEKfyf1VfXO1lBT1ekMk-BuG8OF7gAcHk')
+from config import token
+bot = telebot.TeleBot(token)
 import requests
 from bs4 import BeautifulSoup
 
+HELP_MESSAGE = 'Введи свой знак зодиака и узнай что тебя ждет...'
+UNKNOWN_COMMAND_MESSAGE = 'Звезды не всемогущи, они не могут дать ответ на это(\n Напиши /help.'
+START_MESSAGE = 'Загляни в свое будущее...'
+
+match = {
+    'aries': ['овен', 'aries'],
+    'taurus': ['телец', 'taurus'],
+    'gemini': ['близнец', 'близнецы', 'twin', 'twins', 'gemini'],
+    'cancer': ['рак', 'cancer'],
+    'leo': ['лев', 'leo'],
+    'virgo': ['дева', 'virgo'],
+    'libra': ['весы', 'libra'],
+    'scorpio': ['скорпион', 'scorpio'],
+    'sagittarius': ['стрелец', 'sagittarius'],
+    'capricorn': ['козерог', 'capricorn'],
+    'aquarius': ['водолей', 'aquarius'],
+    'pisces': ['рыбы', 'pisces']
+}
+
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
-    if message.text == "/help":
-        bot.send_message(message.from_user.id, "Введи свой знак зодиака и узнай что тебя ждет...")
-    elif message.text.lower() == "овен" or message.text.lower() == "aries":
-        SITE = 'https://horo.mail.ru/prediction/aries/today/'
-        bot.send_message(message.from_user.id, getHoroscope(SITE))
-    elif message.text.lower() == "телец" or message.text.lower() == "taurus ":
-        SITE = 'https://horo.mail.ru/prediction/taurus/today/'
-        bot.send_message(message.from_user.id, getHoroscope(SITE))
-    elif message.text.lower() == "близнец" or message.text == "близнецы" or message.text == "twins" \
-            or message.text == "twin":
-        SITE = 'https://horo.mail.ru/prediction/gemini/today/'
-        bot.send_message(message.from_user.id, getHoroscope(SITE))
-    elif message.text.lower() == "рак" or message.text == "cancer":
-        SITE = 'https://horo.mail.ru/prediction/cancer/today/'
-        bot.send_message(message.from_user.id, getHoroscope(SITE))
-    elif message.text.lower() == "лев" or message.text.lower() == "leo":
-        SITE = 'https://horo.mail.ru/prediction/leo/today/'
-        bot.send_message(message.from_user.id, getHoroscope(SITE))
-    elif message.text.lower() == "дева" or message.text.lower() == "virgo":
-        SITE = 'https://horo.mail.ru/prediction/virgo/today/'
-        bot.send_message(message.from_user.id, getHoroscope(SITE))
-    elif message.text.lower() == "весы" or message.text.lower() == "libra":
-        SITE = 'https://horo.mail.ru/prediction/libra/today/'
-        bot.send_message(message.from_user.id, getHoroscope(SITE))
-    elif message.text.lower() == "скорпион" or message.text.lower() == "scorpio":
-        SITE = 'https://horo.mail.ru/prediction/scorpio/today/'
-        bot.send_message(message.from_user.id, getHoroscope(SITE))
-    elif message.text.lower() == "стрелец" or message.text.lower() == "sagittarius":
-        SITE = 'https://horo.mail.ru/prediction/sagittarius/today/'
-        bot.send_message(message.from_user.id, getHoroscope(SITE))
-    elif message.text.lower() == "козерог" or message.text.lower() == "capricorn":
-        SITE = 'https://horo.mail.ru/prediction/capricorn/today/'
-        bot.send_message(message.from_user.id, getHoroscope(SITE))
-    elif message.text.lower() == "водолей" or message.text.lower() == "aquarius":
-        SITE = 'https://horo.mail.ru/prediction/aquarius/today/'
-        bot.send_message(message.from_user.id, getHoroscope(SITE))
-    elif message.text.lower() == "рыбы" or message.text.lower() == "pisces":
-        SITE = 'https://horo.mail.ru/prediction/pisces/today/'
-        bot.send_message(message.from_user.id, getHoroscope(SITE))
-    else:
-        bot.send_message(message.from_user.id, "Звезды не всемогущи, они не могут дать ответ на это(\n Напиши /help.")
+    command = message.text.lower()
+    zodiac = None
+    for key, commands in match.items():
+        if command in commands:
+            zodiac = key
+            break
 
-def getHoroscope(SITE):
+    if zodiac is not None:
+        msg = getHoroscope(zodiac)
+    elif command == "/help":
+        msg = HELP_MESSAGE
+    else:
+         msg = UNKNOWN_COMMAND_MESSAGE
+    bot.send_message(message.from_user.id, msg)
+
+
+def getHoroscope(zodiac):
+    SITE = 'https://horo.mail.ru/prediction/' + zodiac + '/today/'
     headers = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:76.0) Gecko/20100101 Firefox/76.0'}
     full_page = requests.get(SITE, headers=headers)
     soup = BeautifulSoup(full_page.content, 'html.parser')
--- Project2/config.py
+++ Project2/config.py
@@ -1,2 +1,1 @@
 token = '1070560085:AAEEKfyf1VfXO1lBT1ekMk-BuG8OF7gAcHk'
-proxy = 'socks5://Proxy_User:Proxy_Password@Proxy_IP:Proxy_Port'
